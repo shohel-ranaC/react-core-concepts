0e6537f07de56cbb39aa59074ac3cf564718fdd0
shohel-ranaC <akashshohel422@gmail.com>
2022-09-14 10:28:40 +0600

    counter and even handler added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const restaurantName = ['Star Kabab', 'Kacchi Vai', 'Crimson Lake'];
+  const ratingStatus = ['Excellent restaurant of the bangladesh', 'Good Quality restaurant in the city', 'Best Restaurant in the world']
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <RestaurantCounter></RestaurantCounter>
+
+        <Restaurant name={restaurantName[0]} rate={ratingStatus[0]}></Restaurant>
+        <Restaurant name={restaurantName[1]} rate={ratingStatus[1]}></Restaurant>
+        <Restaurant name={restaurantName[2]} rate={ratingStatus[2]}></Restaurant>
+        
+
+        {/* <Restaurant name="Star Kabab" rate="Excellent restaurant of the bangladesh"></Restaurant>
+        <Restaurant name="Kacchi Vai" rate="Good Quality restaurant in the city"></Restaurant>
+        <Restaurant name="Crimson Cup" rate="Best Restaurant in the world"></Restaurant>        
+        */}
       </header>
     </div>
   );
+}
+
+function RestaurantCounter(){
+  const [count, setCount] = useState(0);
+  const increaseHandleClick = () => setCount(count + 1);
+  const decreaseHandleClick = () => setCount(count - 1);
+  return (
+    <div>
+      <button onClick={increaseHandleClick}>Add Restaurant</button>
+      <button onClick={decreaseHandleClick}>Delete Restaurant</button>
+      <h3>No. Of Restaurants: {count}</h3>
+      <RestaurantDisplay restaurant={count + 5}></RestaurantDisplay>
+      <RestaurantDisplay restaurant={count + 2}></RestaurantDisplay>
+      <RestaurantDisplay restaurant={count}></RestaurantDisplay>
+    </div>
+  )
+}
+function RestaurantDisplay(props){
+  return <h3>Restaurent In Dhaka City: {props.restaurant}</h3>
+}
+
+function Restaurant(props){
+  const restaurantStyle = {
+    border: '1px solid lightblue',
+    borderRadius: '5px',
+    margin: '10px',
+    width: '500px',
+    backgroundColor: 'orange',
+    color: 'white'
+  }
+  return (
+    <div style={restaurantStyle}>
+      <h2>Restaurant: {props.name}</h2>
+      <p>Rating: {props.rate}</p>
+    </div>
+  )
 }
 
 export default App;
